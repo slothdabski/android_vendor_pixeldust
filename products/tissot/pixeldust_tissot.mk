0f4db594b1e309ebb9aa95002c112c6ba0f48960
spezi77 <spezi7713@gmx.net>
2019-05-20 11:28:13 +0200

    products: Add Release name for tissot.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/tissot/pixeldust_tissot.mk
+++ products/tissot/pixeldust_tissot.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := Mi A1
+export TARGET_DEVICE := tissot
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
