56266f73463c3719cab868847f8d3eb218530343
nitin1438 <nitin.chobhe@gmail.com>
2019-07-09 12:21:40 +0530

    taimen: correct the make file

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -43,8 +43,6 @@ $(call inherit-product, vendor/googleapps/googleapps.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
--include device/google/taimen/device-carbon.mk
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := pixeldust_taimen
 PRODUCT_BRAND := google
