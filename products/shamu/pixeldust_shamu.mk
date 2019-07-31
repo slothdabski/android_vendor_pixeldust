ab9a33a2c9fb87d292496afaae6688b326c72d38
nitin1438 <nitin.chobhe@gmail.com>
2019-08-01 00:01:25 +0530

    Add shamu

---
+++ products/shamu/pixeldust_shamu.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Generic CAF packages
+include device/qcom/common/common.mk
+
+# Include common PixelDust stuff
+include vendor/pixeldust/configs/pixeldust_phone.mk
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/google/moto/device.mk)
+
+# Inherit proprietary files
+$(call inherit-product, vendor/motorola/shamu/shamu-vendor.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := pixeldust_shamu
+PRODUCT_DEVICE := shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27M 4299435 release-keys"
+
+BUILD_FINGERPRINT := google/shamu/shamu:7.1.1/N6F27M/4299435:user/release-keys
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Use SDCLANG
+TARGET_USE_SDCLANG := true
