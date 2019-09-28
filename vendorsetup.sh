AOSPA/android_vendor_pa

vendor: correct Snapdragon LLVM Compiler path
Required for Android Q

Change-Id: Ic554a00c2f8fc44a5b8946c913deb95c52bf5619

vendorsetup.sh
@@ -1,4 +1,4 @@
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/pa/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/pa/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false

slothdabski/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi7713@gmx.net>

vendorsetup.sh
@@ -28,6 +28,6 @@ add_lunch_combo pixeldust_X01BD-userdebug
 add_lunch_combo pixeldust_X01BD-user
 
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/pixeldust/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/pixeldust/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/pixeldust/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/pixeldust/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false